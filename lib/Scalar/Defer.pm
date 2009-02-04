3e69502f480cfa2967e035085e49a1ebf810dd0e
Jesse Vincent <jesse@bestpractical.com>
2009-02-04 18:20:52 +0000

    0.19 built funny. 0.20 is releng changes only

--- lib/Scalar/Defer.pm
+++ lib/Scalar/Defer.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 BEGIN {
-    our $VERSION   = '0.19';
+    our $VERSION   = '0.20';
     our @EXPORT    = qw( lazy defer force );
     our @EXPORT_OK = qw( is_deferred );
 }
